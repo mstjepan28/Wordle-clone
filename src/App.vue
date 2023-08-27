98a4b0ba9785639b07eb7a2895893702b88108fe
Stjepan Markovčić <stjepan.markovcic@superius.hr>
2023-08-27 11:01:27 +0200

    color picker

--- src/App.vue
+++ src/App.vue
@@ -9,9 +9,18 @@
 		</div>
 	</div>
 
+	<div class="colorPicketToggle">
+		<button v-if="colorPickerOpened" type="button" :onClick="() => setColorPickerOpened(false)">
+			<KeyboardIcon />
+		</button>
+		<button v-else type="button" :onClick="() => setColorPickerOpened(true)">
+			<ColorIcon />
+		</button>
+	</div>
+
 	<div class="game-container">
 		<div></div>
-		<div id="countdown" class="game-countdown">Loading...</div>
+		<div id="countdown" class="game-countdown"></div>
 
 		<div class="game-guesses">
 			<div class="guess-instance firstGuess">
@@ -51,24 +60,31 @@
 			</div>
 		</div>
 
-		<OnScreenKeyBoard
-			:totalScores="totalScores"
-			@input="addNewKey"
-			@delete="deleteLastInput"
-			@validate="validateInput"
-		/>
-  	</div>
+		<div v-if="colorPickerOpened">
+			<ColorPicker @changePalette="setColorPalette" />
+		</div>
+		<div v-else>
+			<OnScreenKeyBoard :totalScores="totalScores" @input="addNewKey" @delete="deleteLastInput"
+				@validate="validateInput" />
+		</div>
+	</div>
 </template>
 
 <script>
 import wordList from "./assets/wordList.js";
+import ColorIcon from "./components/ColorIcon.vue";
+import ColorPicker from "./components/ColorPicker.vue";
+import KeyboardIcon from "./components/KeyboardIcon.vue";
 import OnScreenKeyBoard from "./components/onScreenKeyBoard.vue"
 
 export default {
-	components: { OnScreenKeyBoard },
+	components: { OnScreenKeyBoard, KeyboardIcon, ColorIcon, ColorPicker },
 	data() {
-		return{
-			curSession: {word: "humor"},
+		return {
+			colorPickerOpened: false,
+			colorPalette: {},
+
+			curSession: { word: "humor" },
 			coolDown: false,
 
 			countdownTime: 0,
@@ -80,21 +96,37 @@ export default {
 		}
 	},
 	methods: {
-		randInt(min, max){
+		setColorPickerOpened(state) {
+			this.colorPickerOpened = state;
+		},
+
+		setColorPalette(paletteColors) {
+			const defaultPalette = {
+				background: "#171a1b",
+				correct: "#577F45",
+				misplaced: "#c9b458",
+				wrong: "#5e6468",
+			}
+
+			this.colorPalette = paletteColors || defaultPalette;
+			document.body.style.backgroundColor = this.colorPalette.background;
+		},
+
+		randInt(min, max) {
 			return Math.floor(Math.random() * (max - min + 1) + min)
 		},
 
-		formatTimeElapsed(timeElapsed){
+		formatTimeElapsed(timeElapsed) {
 			let minutes = Math.floor(timeElapsed / 60) + "";
 			let seconds = Math.floor(timeElapsed % 60) + "";
 
-			minutes = minutes.split("").length == 1? `0${minutes}`: minutes;
-			seconds = seconds.split("").length == 1? `0${seconds}`: seconds;
+			minutes = minutes.split("").length == 1 ? `0${minutes}` : minutes;
+			seconds = seconds.split("").length == 1 ? `0${seconds}` : seconds;
 
 			return `${minutes}:${seconds}`
 		},
 
-		getRandomWord(){
+		getRandomWord() {
 			const randIndex = this.randInt(0, this.wordList.length - 1);
 
 			this.curSession = {
@@ -110,24 +142,25 @@ export default {
 			this.updateLocalStorage();
 		},
 
-		startNewSession(){
+		startNewSession() {
 			document.getElementById("resultModal").style.display = "none";
 
 			this.getRandomWord();
 			location.reload();
 		},
 
-		terminateSession(isWordGuessed, customMessage){
+		terminateSession(isWordGuessed, customMessage) {
 			clearInterval(this.countDownInterval);
 
 			const timePassed = (this.timeAllowed / 1000) - this.countdownTime
 
-			const gameOutcome = customMessage? customMessage: isWordGuessed? "You got it!": "Game over";
-			const gameResult = isWordGuessed? `Your time: ${this.formatTimeElapsed(timePassed)}`: `The word was: ${this.curSession.word.toUpperCase()}`;
+			const gameOutcome = customMessage ? customMessage : isWordGuessed ? "You got it!" : "Game over";
+			const gameResult = isWordGuessed ? `Your time: ${this.formatTimeElapsed(timePassed)}` : `The word was: ${this.curSession.word.toUpperCase()}`;
 
 			document.getElementById("game-outcome").innerText = gameOutcome;
 			document.getElementById("game-result").innerText = gameResult;
 			document.getElementById("resultModal").style.display = "initial";
+			document.getElementById("resultModal").style.backgroundColor = isWordGuessed? this.colorPalette.correct: this.colorPalette.wrong;
 
 			this.curSession.sessionTerminated = 1;
 			this.updateLocalStorage();
@@ -136,31 +169,37 @@ export default {
 			localStorage.setItem("session", JSON.stringify(this.curSession));
 		},
 
-		startCountdown(timeLeft){
+		startCountdown(timeLeft) {
 			const countDown = document.getElementById("countdown");
 			this.countdownTime = Math.ceil(timeLeft / 1000)
+			countDown.innerText = this.formatTimeElapsed(this.countdownTime);
 
 			this.countDownInterval = setInterval(() => {
 				this.countdownTime -= 1;
 
 				countDown.innerText = this.formatTimeElapsed(this.countdownTime);
 
-				if(countDown == 0) this.terminateSession(false, "Game over - time ran out");
+				if (countDown == 0) {
+					this.terminateSession(false, "Game over - time ran out");
+				}
 			}, 1000)
 		},
 
-		showPreviousInputs(){
+		showPreviousInputs() {
 			this.curSession.guessList.forEach((guess, guessIndex) => {
 				const letterGuesses = this.getGuessDomElement(guessIndex + 1).children;
 
 				guess.guess.forEach((letter, letterIndex) => {
 					let childElement = [...letterGuesses][letterIndex];
+					const score = guess.score[letterIndex];
+					const color = this.colorPalette[score];
+					console.log(score, color)
 
 					childElement.innerText = letter.toUpperCase();
-					childElement.style.background = guess.score[letterIndex];
-					childElement.style.border = `1px solid ${guess.score[letterIndex]}`;
+					childElement.style.background = color;
+					childElement.style.border = `1px solid ${color}`;
 
-					this.totalScores[letter] = guess.score[letterIndex];
+					this.totalScores[letter] = color;
 				})
 			});
 
@@ -172,22 +211,25 @@ export default {
 			})
 		},
 
-		loadSession(){
+		loadSession() {
 			this.curSession = JSON.parse(localStorage.getItem("session"));
 
-			if(!this.curSession || this.curSession?.sessionTerminated == 1)
+			const palette = JSON.parse(localStorage.getItem("colorPalette"));
+			this.setColorPalette(palette);
+
+			if (!this.curSession || this.curSession?.sessionTerminated == 1)
 				return this.startNewSession();
 
 			const timePassed = Date.now() - this.curSession.startTime;
 
-			if(timePassed > this.timeAllowed)
+			if (timePassed > this.timeAllowed)
 				return this.terminateSession(false, "Game over - time ran out");
 
 			this.showPreviousInputs();
 			this.startCountdown(this.timeAllowed - timePassed);
 		},
 
-		getGuessDomElement(guessIndex){
+		getGuessDomElement(guessIndex) {
 			const domElementClass = {
 				1: "firstGuess",
 				2: "secondsGuess",
@@ -201,18 +243,18 @@ export default {
 			return document.querySelector(`div.${className}`);
 		},
 
-		scoreInput(){
+		scoreInput() {
 			const score = [];
 
 			this.curSession.curGuess.forEach((letter, index) => {
 				let newScore = null;
-				
-				if(this.curSession.word[index] == letter)
-					newScore = "#577F45";
-				else if(this.curSession.word.indexOf(letter) != -1)
-					newScore = "#c9b458"; 
+
+				if (this.curSession.word[index] == letter)
+					newScore = "correct"//this.colorPalette.correct || "#577F45";
+				else if (this.curSession.word.indexOf(letter) != -1)
+					newScore = "misplaced"//this.colorPalette.misplaced || "#c9b458";
 				else
-					newScore = "#5e6468";
+					newScore = "wrong"//this.colorPalette.wrong || "#5e6468";
 
 				score.push(newScore);
 			})
@@ -223,27 +265,29 @@ export default {
 			};
 		},
 
-		displayScore(score){
+		displayScore(score) {
 			const letterGuesses = this.getGuessDomElement(false).children;
 
 			score.guess.forEach((letter, letterIndex) => {
 				setTimeout(() => {
 					let childElement = [...letterGuesses][letterIndex];
+					const letterScore = score.score[letterIndex];
+					const color = this.colorPalette[letterScore];
 
 					childElement.innerText = letter.toUpperCase();
-					childElement.style.background = score.score[letterIndex];
-					childElement.style.border = `1px solid ${score.score[letterIndex]}`;
+					childElement.style.background = color;
+					childElement.style.border = `1px solid ${color}`;
 
 					childElement.classList.remove("pop");
 					childElement.classList.add("revealLetter");
 
-					this.totalScores[letter] = score.score[letterIndex];
+					this.totalScores[letter] = color;
 				}, 500 * letterIndex)
 			})
 		},
 
-		addNewKey(newKey){
-			if(this.curSession.curGuess.length >= 5) return;
+		addNewKey(newKey) {
+			if (this.curSession.curGuess.length >= 5) return;
 			this.curSession.curGuess.push(newKey);
 
 			const letterGuesses = this.getGuessDomElement(false).children;
@@ -255,8 +299,8 @@ export default {
 			this.updateLocalStorage();
 		},
 
-		deleteLastInput(){
-			if(this.curSession.curGuess.length == 0) return;
+		deleteLastInput() {
+			if (this.curSession.curGuess.length == 0) return;
 
 			const letterGuesses = this.getGuessDomElement(false).children;
 			const childElement = [...letterGuesses][this.curSession.curGuess.length - 1];
@@ -269,7 +313,7 @@ export default {
 			this.updateLocalStorage();
 		},
 
-		unknownWordHandler(){
+		unknownWordHandler() {
 			console.log("word not in word list!");
 
 			this.coolDown = false;
@@ -277,20 +321,20 @@ export default {
 			return;
 		},
 
-		validateInput(){
-			if(this.coolDown) return;
+		validateInput() {
+			if (this.coolDown) return;
 			this.coolDown = true;
 			setTimeout(() => this.coolDown = false, 2500);
 
-			if(this.curSession.curGuess.length != 5) return;
+			if (this.curSession.curGuess.length != 5) return;
 
 			const wordIndex = this.wordList.indexOf(this.curSession.curGuess.join(""));
-			if(wordIndex == -1) return this.unknownWordHandler();
+			if (wordIndex == -1) return this.unknownWordHandler();
 
 			const score = this.scoreInput();
 			this.displayScore(score);
 
-			if(this.curSession.word == this.curSession.curGuess.join(""))
+			if (this.curSession.word == this.curSession.curGuess.join(""))
 				return setTimeout(() => this.terminateSession(true, null), 2750)
 
 			this.curSession.guessList.push(score)
@@ -298,22 +342,22 @@ export default {
 
 			this.updateLocalStorage();
 
-			if(this.curSession.guessList.length >= 5)
+			if (this.curSession.guessList.length >= 5)
 				this.terminateSession(false, null);
 		},
 
-		updateLocalStorage(){
+		updateLocalStorage() {
 			localStorage.removeItem("session");
 			localStorage.setItem("session", JSON.stringify(this.curSession));
 		},
 
-		keyPressHandler(event){
-			if(this.coolDown) return;
+		keyPressHandler(event) {
+			if (this.coolDown) return;
 
 			const validKeys = "abcdefghijklmnopqrstuvwxyz";
 			const isValidKey = validKeys.indexOf(event.key)
 
-			if(isValidKey != -1) return this.addNewKey(event.key);
+			if (isValidKey != -1) return this.addNewKey(event.key);
 
 			const keyHandler = {
 				Backspace: () => this.deleteLastInput(),
@@ -322,7 +366,7 @@ export default {
 			keyHandler[event.key]?.();
 		}
 	},
-	mounted(){
+	mounted() {
 		this.wordList = wordList.wordList;
 
 		document.addEventListener("keydown", this.keyPressHandler);
@@ -336,185 +380,202 @@ export default {
 <style lang="scss">
 @import "./styles/style.scss";
 
-#app{
+#app {
 	width: 100%;
 	height: 100%;
 }
 
-.modal{
-    width: 100vw;
-    height: 100vh;
+.colorPicketToggle {
+	position: absolute;
+	right: 0;
 
-    position: absolute;
-    top: 0;
-    left: 0;
-    z-index: 10;
+	&>button {
+		padding: 1rem;
 
-    display: none;
+		background: none;
+		outline: none;
+		border: none;
+	}
+}
 
-    background: rgba($gray, 0.5);
-    
-    .modal-body{
-        width: 75%;
-        min-width: 400px;
-        max-width: 750px;
-    
-        min-height: 250px;
-    
-        font-size: 36px;
-        font-weight: bold;
-        color: white;
-    
-        display: flex;
-        justify-content: center;
-        align-items: center;
-        flex-direction: column;
-        row-gap: 3rem;
-    
-        padding: 2rem 0;
-    
-        position: absolute;
-        top: 50%;
-        left: 50%;
-        transform: translate(-50%, -50%);
-    
-        border: 1px solid $white;
-        background: $gray-dark;
+.modal {
+	width: 100vw;
+	height: 100vh;
 
-		h2, h3{
+	position: absolute;
+	top: 0;
+	left: 0;
+	z-index: 10;
+
+	display: none;
+
+	background: rgba($gray, 0.5);
+
+	.modal-body {
+		width: 75%;
+		min-width: 400px;
+		max-width: 750px;
+
+		min-height: 250px;
+
+		font-size: 36px;
+		font-weight: bold;
+		color: white;
+
+		display: flex;
+		justify-content: center;
+		align-items: center;
+		flex-direction: column;
+		row-gap: 3rem;
+
+		padding: 2rem 0;
+
+		position: absolute;
+		top: 50%;
+		left: 50%;
+		transform: translate(-50%, -50%);
+
+		h2,
+		h3 {
 			text-align: center;
 		}
-    
-        .playAgainButton{
-            margin-top: 1.5rem;
-            padding: 0 1rem 0.25rem 1rem;
-    
-            font-size: 36px;
-            font-weight: bold;
-            color: white;
-    
-            cursor: pointer;
-            border: none;
-    
-            background: none;
-            &:focus, &:hover{
-                border-bottom: 1px solid white;
-                outline: none;
-            }
-        }
-    }
+
+		.playAgainButton {
+			margin-top: 1.5rem;
+			padding: 0 1rem 0.25rem 1rem;
+
+			font-size: 36px;
+			font-weight: bold;
+			color: white;
+
+			cursor: pointer;
+			border: none;
+
+			background: none;
+
+			&:focus,
+			&:hover {
+				border-bottom: 1px solid white;
+				outline: none;
+			}
+		}
+	}
 }
 
-.pop{
-    animation: expandAnimation 0.125s;
+.pop {
+	animation: expandAnimation 0.125s;
 
-    @keyframes expandAnimation {
-        0%{
-            transform: scale(1);
-        }
-        50%{
-            transform: scale(1.25);
-        }    
-        100%{
-            transform: scale(1);
-        }    
-    }
+	@keyframes expandAnimation {
+		0% {
+			transform: scale(1);
+		}
+
+		50% {
+			transform: scale(1.25);
+		}
+
+		100% {
+			transform: scale(1);
+		}
+	}
 }
 
-.revealLetter{
-    animation: revealLetterAnimation 0.75s cubic-bezier(.01, 1.36, .77, .69) both;
+.revealLetter {
+	animation: revealLetterAnimation 0.75s cubic-bezier(.01, 1.36, .77, .69) both;
 
-    @keyframes revealLetterAnimation {
-        0% {
-            transform: translateY(0) scale(1);
-        }
-        50% {
-            transform: translateY(-1rem) scale(1.1);
-        }
-        100% {
-            transform: translateY(0) scale(1);
-        }
-    }
+	@keyframes revealLetterAnimation {
+		0% {
+			transform: translateY(0) scale(1);
+		}
+
+		50% {
+			transform: translateY(-1rem) scale(1.1);
+		}
+
+		100% {
+			transform: translateY(0) scale(1);
+		}
+	}
 }
 
-.game-container{
-    height: 100%;
+.game-container {
+	height: 100%;
 
-    display: flex;
-    justify-content: center;
-    flex-direction: column;
+	display: flex;
+	justify-content: center;
+	flex-direction: column;
 
-    .game-countdown{
-        margin: 0 auto;
+	.game-countdown {
+		margin: 0 auto;
 
-        font-size: 48px;
-        color: white;
-    }
+		font-size: 48px;
+		color: white;
+	}
 
-    .game-guesses{
-        display: flex;
-        align-items: center;
-        flex-direction: column;
-        
-        gap: 1.5rem;
+	.game-guesses {
+		display: flex;
+		align-items: center;
+		flex-direction: column;
+
+		gap: 1.5rem;
 		margin: 4rem 0;
-        
-        .guess-instance{
-            display: flex;
-            justify-content: space-between;
 
-            gap: 1rem;
+		.guess-instance {
+			display: flex;
+			justify-content: space-between;
 
-            .guess-letter{
-                --letterBox-size: 75px;
+			gap: 1rem;
+
+			.guess-letter {
+				--letterBox-size: 75px;
 				--letterBox-font: 40px;
 
-                width: 75px;
-                aspect-ratio: 1/1;
+				width: 75px;
+				aspect-ratio: 1/1;
 
-                display: flex;
-                justify-content: center;
-                align-items: center;
+				display: flex;
+				justify-content: center;
+				align-items: center;
 
-                font-size: 40px;
-                font-weight: bold;
-                color: white;
-    
-                border: 1px solid $white;
-            }
-        }
-    }
+				font-size: 40px;
+				font-weight: bold;
+				color: white;
+
+				border: 1px solid $white;
+			}
+		}
+	}
 }
 
 @media only screen and (max-width: 650px) {
-	.guess-letter{
+	.guess-letter {
 		width: 50px !important;
 		font-size: 30px !important;
 	}
 }
+
 @media only screen and (max-width: 650px) {
-	.guess-letter{
+	.guess-letter {
 		width: 50px !important;
 		font-size: 30px !important;
 	}
 }
 
 @media only screen and (max-height: 700px) {
-	.game-guesses{
+	.game-guesses {
 		margin: 2rem 0 !important;
 		gap: 0.75rem !important;
 
-		.guess-instance{
+		.guess-instance {
 			gap: 0.25rem !important;
 		}
 	}
 }
 
 @media only screen and (max-height: 600px) {
-	.game-guesses{
+	.game-guesses {
 		margin: 0.5rem !important;
 		gap: 0.75rem !important;
 	}
 }
-
 </style>
---
+++ src/components/ColorPicker.vue
@@ -0,0 +1,71 @@
+<template>
+  <div class="colorPickerContainer">
+    <button class="blackColor" type="button" :onClick="() => setColorPalette('black')">Black</button>
+    <button class="pinkColor" type="button" :onClick="() => setColorPalette('pink')">Pink</button>
+  </div>
+</template>
+
+<script>
+export default {
+  methods: {
+    setColorPalette(colorPalette) {
+      const palette = {
+        pink: {
+          background: "#f4acb7",
+          correct: "#d8e2dc",
+          misplaced: "#9d8189",
+          wrong: "#ffe5d9",
+        },
+        black: {
+          background: "#171a1b",
+          correct: "#577F45",
+          misplaced: "#c9b458",
+          wrong: "#5e6468",
+        }
+      }[colorPalette];
+
+      if(!palette) {
+        return;
+      }
+
+      localStorage.setItem("colorPalette", JSON.stringify(palette))
+      location.reload()
+    } 
+  }
+}
+</script>
+
+<style lang="scss">
+@import "../styles/style.scss";
+
+.colorPickerContainer {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  column-gap: 2rem;
+
+	& > button {
+    min-width: 6rem;
+    font-size: 20px;
+    font-weight: 500;
+		padding: 0.5rem;
+
+    border: 2px solid white;
+    border-radius: 100px;
+
+		outline: none;
+    cursor: pointer;
+	}
+}
+
+.blackColor {
+  color: white;
+  background: #171a1b;
+}
+
+.pinkColor {
+  color: white;
+  background: pink;
+}
+
+</style>
